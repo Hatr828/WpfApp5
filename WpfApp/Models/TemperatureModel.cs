d12efcb8c5450a30cdcc49e49d7e11864d5e20c4
Hatr727 <hatr626@gmail.com>
2024-06-25 21:32:32 -0500

    0

---
+++ WpfApp/Models/TemperatureModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TemperatureConverter.Models
+{
+    public class TemperatureModel
+    {
+        private double _celsius;
+
+        public double Celsius
+        {
+            get { return _celsius; }
+            set
+            {
+                _celsius = value;
+                OnPropertyChanged(nameof(Celsius));
+            }
+        }
+
+        public double Fahrenheit => _celsius * 9 / 5 + 32;
+
+        public double Kelvin => _celsius + 273.15;
+
+        public event EventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfApp/ViewModels/TemperatureViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using TemperatureConverter.Models;
+using WpfApp.Command;
+
+namespace TemperatureConverter.ViewModels
+{
+    public class TemperatureViewModel : INotifyPropertyChanged
+    {
+        private TemperatureModel _model;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand ConvertCommand { get; }
+
+        public TemperatureViewModel()
+        {
+            _model = new TemperatureModel();
+            ConvertCommand = new RelayCommand(Convert);
+        }
+
+        public double Celsius
+        {
+            get { return _model.Celsius; }
+            set
+            {
+                _model.Celsius = value;
+                OnPropertyChanged(nameof(Celsius));
+                OnPropertyChanged(nameof(Fahrenheit));
+                OnPropertyChanged(nameof(Kelvin));
+            }
+        }
+
+        public double Fahrenheit => _model.Fahrenheit;
+
+        public double Kelvin => _model.Kelvin;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Convert()
+        {
+            OnPropertyChanged(nameof(Fahrenheit));
+            OnPropertyChanged(nameof(Kelvin));
+        }
+    }
+}
